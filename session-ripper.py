4c05a11d4ce956dcab31a45d77fa57a837bc2a87
Fennel <fennel@everwild.dev>
2024-12-03 21:10:50 -0500

    second

--- session-ripper.py
+++ session-ripper.py
@@ -9,6 +9,6 @@ for i in range(1, 25054):
     if abc.status_code != 200:
         print(f"Tune {i} not found on site! Error code: {abc.status_code}")
         continue
-    tune_title = re.search(r"T: (.*)", abc.text).group(1)
+    tune_title = re.search(r"T: (.*)", abc.text).group(1).strip()
     with open(f"data/{i}-{tune_title}.abc", "w") as f:
         f.write(abc.text)
