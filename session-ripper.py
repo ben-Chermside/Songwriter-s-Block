2588bd76165c4a4d48171ee8cc5c6de61d4caba6
Fennel <fennel@everwild.dev>
2024-12-03 21:02:31 -0500

    added session ripper

---
+++ session-ripper.py
@@ -0,0 +1,14 @@
+import requests
+import re
+import time
+
+for i in range(1, 25054):
+    time.sleep(1)
+    print(f"Ripping file {i}: {(i/25053) * 100:.3f}%")
+    abc = requests.get(f"https://thesession.org/tunes/{i}/abc")
+    if abc.status_code != 200:
+        print(f"Tune {i} not found on site! Error code: {abc.status_code}")
+        continue
+    tune_title = re.search(r"T: (.*)", abc.text).group(1)
+    with open(f"data/{i}-{tune_title}.abc", "w") as f:
+        f.write(abc.text)
