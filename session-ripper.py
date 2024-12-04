3784d4f0702920708d5f4cf08fd4a1516a96dde6
Fennel <fennel@everwild.dev>
2024-12-04 03:09:14 -0500

    added proper progress bar to session-ripper

--- session-ripper.py
+++ session-ripper.py
@@ -1,14 +1,28 @@
 import requests
 import re
 import time
+import argparse
+from tqdm import trange
 
-for i in range(1, 25054):
+parser = argparse.ArgumentParser()
+parser.add_argument("num", type=int, default=1)
+
+args = parser.parse_args()
+
+ran = trange(args.num, 25054)
+for i in ran:
     time.sleep(1)
-    print(f"Ripping file {i}: {(i/25053) * 100:.3f}%")
     abc = requests.get(f"https://thesession.org/tunes/{i}/abc")
     if abc.status_code != 200:
-        print(f"Tune {i} not found on site! Error code: {abc.status_code}")
+        ran.write(f"Tune {i} not found on site! Error code: {abc.status_code}")
         continue
-    tune_title = re.search(r"T: (.*)", abc.text).group(1).strip()
-    with open(f"data/session/{i}-{tune_title}.abc", "w") as f:
-        f.write(abc.text)
+    tune_title = re.search(r"T: (.*)", abc.text)
+    if tune_title is None:
+        ran.write(f"Tune {i} title not parseable!")
+        continue
+    tune_title = tune_title.group(1).strip()
+    try:
+        with open(f"data/session/{i}-{tune_title}.abc", "w") as f:
+            f.write(abc.text)
+    except FileNotFoundError as e:
+        ran.write(f"Failed to rip {i}: {e}")
