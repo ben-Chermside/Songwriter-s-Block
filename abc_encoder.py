edc22416ad09498787aa83cac5300091371f4a28
AbeFrato <abefrato@gmail.com>
2024-12-15 14:48:00 -0500

    Progress on parser but very non-functional

--- abc_encoder.py
+++ abc_encoder.py
@@ -23,68 +23,126 @@ def parse_abc(path):
     song_type = ''
     genre = ''
 
-    key = '' # for transposition purposes
+    key = 0 # for transposition purposes
 
     notes = []
 
+    note_mapping = {
+        "C": 0,
+        "D": 2,
+        "E": 4,
+        "F": 5,
+        "G": 7,
+        "A": 9,
+        "B": 11,
+        "H": 11,
+        }
+
     for line in file:
+        print(line)
         # song type
         if line.startswith('R:'):
             line = line.replace(' ', '')
             song_type = line[2:]
         # time signature
-        if line.startswith('M:'):
+        elif line.startswith('M:'):
             line = line.replace(' ', '')
             time_signature = line[2:]
         # key and mode
-        if line.startswith('K:'):
+        elif line.startswith('K:'):
             line = line.replace(' ', '')
             line = line.lower()
 
-            key = line[3]
+            key = note_mapping[line[3].upper()]
 
-            if len(line[2:]) == 1:
+            if line[4] == '#':
+                key = (key + 1) % 12
+            if line[4] == 'b':
+                key = (key - 1) % 12
+
+            if 1 <= len(line[2:]) <= 2:
                 mode = 'major'
-            elif line[2:] == 'min' or line[2:] == 'm':
+            elif 'min' in line or 'm' in line:
                 mode = 'minor'
-            elif line[2:] == 'dor':
+            elif 'dor' in line:
                 mode = 'dorian'
-            elif line[2:] == 'phr':
+            elif 'phr' in line:
                 mode = 'phrygian'
-            elif line[2:] == 'lyd':
+            elif 'lyd' in line:
                 mode = 'lydian'
-            elif line[2:] == 'mix':
+            elif 'mix' in line:
                 mode = 'mixolydian'
-            elif line[2:] == 'loc':
+            elif 'loc' in line:
                 mode = 'loc'
             else:
                 mode = 'unspecified'
+        elif len(line) > 1 and  (line[1] != ':' or line.startswith('|:')) and (len(line) != 0 and "utf-8" not in line):
+            adjustment = 0
 
-        note = ''
-        duration = ''
-        octave = 0
-
-        if (line[2] != ':' or line.startswith('|:')) and (len(line) != 0 and not line.endswith("utf-8")):
             i = 0
             while i < len(line):
+
+                note = 0
+                duration = 0
+                octave = 0
+
                 c = line[i]
 
                 if c == ' ':
                     i += 1
-                    continue
                     
-                if c.isalpha():
-                    note += c
-                    if c.islower():
-                        octave += 1
-                    elif c.isupper():
-                        octave -= 1
+                # barlines and repeats
+                elif c in '|:':
+                    if i + 1 < len(line) and line[i + 1] in ':|':
+                        # repeats
+                        symbol = c + line[i + 1]
+                        notes.append([symbol, 0, 0])  
+                        i += 1  
+                    else:
+                        # single bar line
+                        notes.append([c, 0, 0])
                 
+                # accidentals
+                elif c in "^_=":
+                    if c == "^":
+                        adjustment += 1
+                    elif c == "_":
+                        adjustment -= 1
+                    elif c == '=':
+                        adjustment = 0
+                    i += 1
+
+                # notes
+                elif c.isalpha() and c.upper() in note_mapping:
+                    note = (note_mapping[c.upper()] + adjustment) % 12                    
+                    duration = 1
+                    
+                    if c.isupper():
+                        octave = -1
+                    else:
+                        octave = 0
+
+                    # extra octave adjustments
+                    while i + 1 < len(line) and line[i + 1] in ["'", ","]:
+                        if line[i + 1] == "'":
+                            octave += 1
+                        elif line[i + 1] == ",":
+                            octave -= 1
+                        i += 1
+                    
+                    # duration
+                    while i + 1 < len(line) and (line[i + 1].isdigit() or line[i + 1] == "/"):
+                        i += 1
+                        if line[i] == "/":
+                            duration /= 2 
+                        elif line[i].isdigit():
+                            duration *= int(line[i])
 
                 # add encoded note
                 notes.append([note, duration, octave])
-
+                adjustment
                 i += 1
+
             return {
                 "time_signature": time_signature,
                 "mode": mode,
