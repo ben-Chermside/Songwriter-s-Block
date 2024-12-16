72f4675d018097cd60fd06bc9fbfcc4ab6512d3f
Fennel <fennel@everwild.dev>
2024-12-16 18:04:12 -0500

    added reverse to token_to_int

--- token_to_int.py
+++ token_to_int.py
@@ -1,9 +1,10 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('token_csv', type=argparse.FileType('r', encoding='latin-1'), help="Path to token csv file")
+parser.add_argument('input_file', type=argparse.FileType('r', encoding='latin-1'), help="Path to token csv file")
 parser.add_argument('translation_file', type=argparse.FileType('w+'), help="Path to translation file")
 parser.add_argument('output_file', type=argparse.FileType('w'), help="Path to output file")
+parser.add_argument('-r', '--reverse', action='store_true')
 
 args = parser.parse_args()
 
@@ -14,18 +15,23 @@ translation_file = args.translation_file.readlines()
 for line in translation_file:
     translation.append(line)
 
-token_csv = args.token_csv.readlines()
-for line in token_csv:
+input_file = args.input_file.readlines()
+
+for line in input_file:
     line_tokens = line.split(',')
     for token in line_tokens:
         token = token.strip()
-        try:
-            translated_int = translation.index(token)
-        except ValueError:
-            translation.append(token)
-            translated_int = len(translation) - 1
+        if args.reverse:
+            # This will throw an uncaught IndexError if an integer does not exist in the translation
+            translated = translation[token]
+        else:
+            try:
+                translated = translation.index(token)
+            except ValueError:
+                translation.append(token)
+                translated = len(translation) - 1
         # Write int, to new file
-        output_file.write(str(translated_int) + ', ')
+        output_file.write(str(translated) + ', ')
     output_file.write('\n')
 
 # Dump new translation file to same pathname
@@ -35,6 +41,7 @@ translation_file.seek(0)
 for item in translation:
     translation_file.write(item + '\n')
 
+
 translation_file.close()
 output_file.close()
-args.token_csv.close()
+args.input_file.close()
